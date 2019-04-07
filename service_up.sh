fab40ceb46c282bf75abdc0b7632b9e7e7af8868
Eliezer Yucht <Eliezer.Yucht@harman.com>
2019-04-07 23:22:14 +0300

    Add service_up.sh

--- service_up.sh
+++ service_up.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+cd /home/pi/Hackathon/hackathon_generator
 sh car_files/can_down.sh
 sh car_files/setup.sh
 sh car_files/can_up.sh
-python3 bio_car_auth/auth_server.py 192.168.10.215
+cd /home/pi/Hackathon/hackathon_generator/bio_car_auth
+sudo python3 auth_server.py 192.168.100.215

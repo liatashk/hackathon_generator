da120fb26d4781034943eda4014fbdd4e328de8d
David Pilnik <dpilnik@tower-sec.com>
2019-04-07 14:30:46 +0300

    start develop sent msg to can bus

---
+++ car.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import can
+
+
+
+def send_one():
+    bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
+    #bus = can.interface.Bus(bustype='ixxat', channel=0, bitrate=250000)
+    #bus = can.interface.Bus(bustype='vector', app_name='CANalyzer', channel=0, bitrate=250000)
+
+    msg = can.Message(arbitration_id=0xc0ffee,
+                      data=[0, 25, 0, 1, 3, 1, 4, 1],
+                      extended_id=True)
+    try:
+        bus.send(msg)
+        print("Message sent on {}".format(bus.channel_info))
+    except can.CanError:
+        print("Message NOT sent")
+
+if __name__ == "__main__":
+    send_one()
---
+++ car_files/setup.sh
@@ -0,0 +1,1 @@
+sudo ip link set can0 type can bitrate 500000
